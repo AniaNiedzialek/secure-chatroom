0369bc023ab087b2ad7e5dd459db901f46c2518f
Sunny Doan <flabbhs@gmail.com>
2025-04-20 21:21:42 -0700

    Add AES encryption to client and update documentation

--- client/basic_client.py
+++ client/basic_client.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+from crypto_utils import encrypt_message, decrypt_message
+
 HOST = input("Enter server IP address (run 'ifconfig lo0' command if unsure: ")
 PORT = 12345 # change to interactively ask for the port as well?
 
@@ -8,8 +10,12 @@ username = input("Enter your username: ")
 def receive_messages(sock):
     while True:
         try:
-            msg = sock.recv(1024).decode('utf-8')
-            print(f"\r{msg}\n[{username}] > ", end="")
+            encrypted = sock.recv(1024)
+            try:
+                msg = decrypt_message(encrypted)
+                print(f"\r{msg}\n[{username}] > ", end="")
+            except:
+                print("\n[!] Failed to decrypt message.")
         except:
             print("Disconnected from server.")
             break
@@ -17,18 +23,18 @@ def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
     print("Connected to server.")
-    
+
     thread = threading.Thread(target=receive_messages, args=(client,))
     thread.start()
-    
+
     while True:
         msg = input(f"[{username}] > ")
         if msg.lower() == "exit":
-            client.send(f"[{username}] has left the chat.".encode('utf-8'))
+            encrypted = encrypt_message(f"[{username}]: {msg}")
+            client.send(encrypted)
             client.close()
             break
         client.send(f"[{username}]: {msg}".encode('utf-8'))
 
-        
+
 start_client()
-    
