87d1d10bd61b4b032930dec24a2d9ee52540562f
Ania <ania.niedzialek@sjsu.edu>
2025-04-21 15:45:18 -0700

    Secure login system added with SHA-256 and username handling

--- client/basic_client.py
+++ client/basic_client.py
@@ -1,11 +1,58 @@
 import socket
 import threading
+
 from crypto_utils import encrypt_message, decrypt_message
+
+import json
+import hashlib
+import os
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def load_users():
+    if not os.path.exists("users.json"):
+        return {}
+    with open("users.json", "r") as f:
+        return json.load(f)
+    
+def save_users(users):
+    with open("users.json", "w") as f:
+        json.dump(users, f, indent=4)
+
+users = load_users()
+print("[1] Login")
+print("[2] Register")
+choice = input("Choose an option: ")
+
+while choice not in ["1", "2"]:
+    choice = input("Please type 1 or 2: ")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+hashed = hash_password(password)
+
+if choice == "1":
+    if username not in users or users[username] != hashed:
+        print("Invalid username or password! Exiting")
+        exit()
+    else:
+        print("Login successful!")
+else:
+    if username in users:
+        print("Username already exists! Exiting..")
+        exit()
+    users[username] = hashed
+    save_users(users)
+    print("Registration was successful! Welcome ")        
+
+
 
 HOST = input("Enter server IP address (run 'ifconfig lo0' command if unsure: ")
 PORT = 12345 # change to interactively ask for the port as well?
 
-username = input("Enter your username: ")
+
+
+# username = input("Enter your username: ")
 
 def receive_messages(sock):
     while True:
