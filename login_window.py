3835539b11244e444dcf0362a417ca66e2039413
Ania <ania.niedzialek@sjsu.edu>
2025-05-03 17:56:23 -0700

    work in progress - added ip and port input

--- login_window.py
+++ login_window.py
@@ -26,13 +26,88 @@ def load_users():
 class SignalWrapper(QObject):
     message_received = pyqtSignal(str)
  
-class LoginWindow(QWidget):
+class ConnectionWindow(QWidget):
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle("SafeChat")
+        self.setWindowTitle("Connect to Server")
         self.setGeometry(200,200, 400, 300)
         
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignCenter)
+        
+        self.ip_input = QLineEdit()
+        self.ip_input.setPlaceholderText("Server IP Addres")
+        layout.addWidget(self.ip_input)
+        
+        self.port_input = QLineEdit()
+        self.port_input.setPlaceholderText("Port Number")
+        layout.addWidget(self.port_input)
+        
+        self.connect_button = QPushButton("Next")
+        self.connect_button.clicked.connect(self.proceed_to_login)
+        layout.addWidget(self.connect_button)
+        
+        self.setLayout(layout)
+        
+        self.setStyleSheet("""
+        QWidget{
+            background-color: #442b54;
+                color: #ffffff;
+                    font-family: 'Helvetica Neue';
+                    font-size: 13px;
+        }
+        
+        QTextEdit {
+            background-color: #6d4d6e;
+                border: 1px solid #3e3e55;
+                padding: 8px;
+                color: #d0d0ff;
+        }
+        
+        QLineEdit {
+            background-color: #6d4d6e;
+                border: 1px solid #444466;
+                padding: 6px;
+                color: #e0e0ff;
+        }
+        
+        QPushButton {
+            background-color: #6b5e7d;
+            color: white;
+            border-radius:6x;
+            padding: 6px;
+        }
+        
+        QPushButton:hover {
+            background-color: #e667b8;
+                
+        }
+        
+        """)
+    
+    def proceed_to_login(self):
+        ip = self.ip_input.text().strip()
+        port = int(self.port_input.text().strip())
+        
+        if not ip or not port:
+            QMessageBox.warning(self, "Error", "Enter a valid IP and port.")
+            return
+        
+        self.login_window = LoginWindow(ip, port)
+        self.login_window.show()
+        self.close()
+        
+        
+ 
+class LoginWindow(QWidget):
+    def __init__(self, ip, port):
+        super().__init__()
+        self.ip = ip
+        self.port = port
+                
+        self.setWindowTitle("SafeChat")
+        self.setGeometry(200,200, 400, 300)
         
         layout = QVBoxLayout()
         
@@ -100,6 +175,11 @@ class LoginWindow(QWidget):
         """)
         
     def attempt_login(self):
+        # ip = self.ip
+        # port = self.port
+
+        # self.chat_window = ChatWindow(username, self.ip, self.port, self)
+        
         username = self.username_input.text().strip()
         password = self.password_input.text().strip()
         
@@ -109,7 +189,7 @@ class LoginWindow(QWidget):
         if username in users and users[username] == hashed:
             self.label.setText(f"Login successful! Welcome to Chatroom, {username}!")
             
-            self.chat_window = ChatWindow(username, self)
+            self.chat_window = ChatWindow(username, self.ip, self.port, self)
             self.chat_window.show()
             self.hide()
         else:
@@ -137,14 +217,28 @@ import socket
 import threading
 from client.crypto_utils import encrypt_message, decrypt_message
 
-HOST = "127.0.0.1"
-PORT = 12345 #8080 for MITM attack
+# HOST = "127.0.0.1"
+# PORT = 12345 #8080 for MITM attack
 
 
 class ChatWindow(QWidget):
-    def __init__(self, username, login_window=None):
+    def __init__(self, username, ip, port, login_window=None):
         super().__init__()
+        self.running = True
+        
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client_socket.connect((ip, port))
+            self.connected = True
+            print("[Client] connected to the server.")
+        except Exception as e:
+            self.connected = False
+            QMessageBox.critical(self, "Connection Failed", f"Could not connect to the {ip}:{port}\nError: {str(e)}")
+            self.close()
+            return
         self.username = username
+        self.ip = ip
+        self.port = port
         self.login_window = login_window
         self.encryption_enabled = True  # Default to enabled
         
@@ -182,18 +276,6 @@ class ChatWindow(QWidget):
         layout.addLayout(input_layout)
         self.setLayout(layout)
         
-        # socekts
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       
-        try :
-            self.client_socket.connect((HOST, PORT))
-            self.connected = True
-            print("[Client] Connected to proxy!")
-        except:
-            self.connected = False
-            QMessageBox.critical(self, "Connection Error", "Cannot connect to the server.\nMake sure it is running...")
-            self.close()
-            return    
 
         receive_thread = threading.Thread(target=self.receive_message)
         receive_thread.daemon = True
@@ -273,9 +355,9 @@ class ChatWindow(QWidget):
                 self.message_input.clear()
             except Exception as e:
                 QMessageBox.critical(self, "Error", f"Failed to send message: {str(e)}")
-    
+
     def receive_message(self):
-        while self.connected:
+        while self.running and self.connected:
             try:
                 data = self.client_socket.recv(4096)
                 if not data:
@@ -294,18 +376,19 @@ class ChatWindow(QWidget):
                     self.signals.message_received.emit(message)
                     
             except Exception as e:
-                print(f"Error receiving message: {e}")
+                if self.running:
+                    print(f"Error receiving message: {e}")
                 break
-            
+           
     def logout(self):
+        self.running = False
+        self.connected = False
         self.client_socket.close()
         self.close()
         
-        self.login_window = LoginWindow()
+        self.login_window = LoginWindow(self.ip, self.port)
         self.login_window.show()
-        if self.login_window:
-            self.login_window.show()
-            
+
     def display_message(self, message):
         if message.startswith(f"[{self.username}]"):
             formatted = f'<span style="color:#d7aaff;">{message}</span>'
@@ -323,6 +406,6 @@ class ChatWindow(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = LoginWindow()
+    window = ConnectionWindow()
     window.show()
     sys.exit(app.exec_())
